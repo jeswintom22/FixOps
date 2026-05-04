b62a26d3378469ccbe8787120b07804690317904
jeswintom22 <jeswintom8@gmail.com>
2026-05-04 10:13:37 +0530

    Step 2:Implement ingestion service for log processing and add endpoint to retrieve logs

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from typing import List,Optional
+import uuid
+from datetime import datetime
 
 app = FastAPI()
+logs_db =[]
 
+def ingestion_service(logs):   #ingestion service(logic layer)
+    processed_logs=[]
+
+    for log in logs:
+        log_dict=log.dict()
+        if not log_dict.get("trace_id"):      #attach trace_id
+            log_dict["trace_id"]=str(uuid.uuid4())
+        
+        if not log_dict.get("timestamp"):    #normalize timestamp
+            log_dict["timestamp"]=datetime.utcnow().isoformat()
+        processed_logs.append(log_dict)
+    logs_db.extend(processed_logs)
+    return processed_logs
 class Log(BaseModel):
     timestamp:Optional[str]=None
     level:str="INFO"
@@ -15,6 +31,19 @@ class Log(BaseModel):
 class LogRequest(BaseModel):
     logs:List[Log]
 
-@app.post("/test")
-async def test_schema(payload:LogRequest):
-    return payload
+@app.get("/logs")                    #debug point
+def get_logs():
+    return {
+        "total":len(logs_db),
+        "logs":logs_db
+    }
+
+@app.post("/ingest")                               #real ingestion endpoint
+async def ingest_logs(payload: LogRequest):
+    processed = ingestion_service(payload.logs)
+
+    return {
+        "status":"ingested",
+        "count":len(processed),
+        "logs":processed
+    }
